3f7aac180cd32e587c15e651836031eaddb96283
tarkeshwarraj <tarkeshwarraj@gmail.com>
2024-08-18 15:50:58 +0530

    link update

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -10,7 +10,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 //placing user order from frontend
 
 const placeOrder = async (req, res) => {
-  const frontend_url = "http://localhost:5174";
+  const frontend_url = "https://fooddelivery-mern-fullstack-1.onrender.com";
 
   try {
     //creating new order
